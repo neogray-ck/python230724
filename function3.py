0d15daff5d894368f435e9d5148bd0410540a7aa
neogray-ck <neogray@gmail.com>
2023-07-25 10:49:50 +0900

    수열함수 연습

--- function3.py
+++ function3.py
@@ -12,3 +12,26 @@ def union(*ar):
 #호출
 print(union("HAM", "SPAM"))
 print(union("HAM", "SPAM", "EGG"))
+
+#람다 함수
+g = lambda x,y:x*y
+print(g(3,4))
+print(g(5,6))
+print((lambda x:x*x)(3))
+print(globals())
+
+#내장함수 필터
+#필터링 함수 정의
+def getBiggerTans20(i):
+    return i>20
+
+print("---TEST1---")
+lst = [10, 25, 30]
+iterL = filter(getBiggerTans20, lst)
+for i in iterL:
+    print(i)
+
+print("---람다함수---")
+iterL = filter(lambda x:x>20, lst)
+for i in iterL:
+    print(i)
