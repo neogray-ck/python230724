4aa628c8acbe334242fc60aa2f50f5c68d3134e1
neogray-ck <neogray@gmail.com>
2023-07-26 09:24:03 +0900

    0726

---
+++ DemoFile.py
@@ -0,0 +1,5 @@
+# DemoFile.py
+for i in range(0, 10):
+    url = "http://www.credut.com/?page=" + str(i)
+    print(url)
+    
--- 상속01.py
+++ 상속01.py
@@ -2,9 +2,13 @@ class Person:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name,
+                                                         self.phoneNumber))
+    def working(self):
+        print("현재 작업중")
+    def coding(self):
+        print("현재 코딩중")
 
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
@@ -12,11 +16,16 @@ class Student(Person):
         self.phoneNumber = phoneNumber
         self.subject = subject
         self.studentID = studentID
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name,
+                                                         self.phoneNumber))
+        print("Info(Subject:{0}, StudentID: {1})".format(self.subject,
+                                                         self.studentID))
 
-
+#인스턴스
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
-print(p.__dict__)
-print(s.__dict__)
+p.printInfo()
+s.printInfo()
 
-
+s.coding()
